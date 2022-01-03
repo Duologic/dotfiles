8faa6aa104c8e9d174d7aab8344d2e3fe207d55f
Duologic <jeroen@simplistic.be>
2022-01-03 08:36:06 +0100

    update firefox user.js

--- mozilla/firefox/user.js
+++ mozilla/firefox/user.js
@@ -3,7 +3,7 @@ user_pref("accessibility.force_disabled", 1);
 user_pref("browser.bookmarks.restore_default_bookmarks", false);
 user_pref("browser.ssb.enabled", true);
 user_pref("datareporting.healthreport.uploadEnabled", false);
-user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.pocket.enabled", true);
 user_pref("findbar.highlightAll", true);
 user_pref("media.hardwaremediakeys.enabled", false);
 user_pref("privacy.donottrackheader.enabled", true);
